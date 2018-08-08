786e3ddec5cc88b874b104bfcf377cfe77804a09
Piyush Meharban <p.meharban@yahoo.com>
2018-08-08 22:16:48 +0530

    added step2 testcase2 for pdf

---
+++ __tests__/step2_case2.test.js
@@ -0,0 +1,49 @@
+const puppeteer = require('puppeteer');
+jest.setTimeout(50000);
+describe('Open Google', () => {
+  var browser, page;
+  var url = 'https://google.com'
+beforeEach (async () => {
+    browser = await puppeteer.launch({ headless: false });
+    page = await browser.newPage();
+  })
+afterEach (() => {
+    browser.close()
+  })
+
+ //Opening the Page and entering the search query 
+test('Title == google', async () => {
+    await page.goto(url);   
+    const title = await page.title();
+    expect(title).toBe("Google");
+    await page.click("input[name=q]");
+    var file = "pdf adobe"
+    await page.type("input[name=q]", "filetype:".concat(file));
+    await page.evaluate(async() => {
+      await new Promise(function(resolve) { 
+             setTimeout(resolve, 1000)
+      });
+  });
+    await page.click('input[type=submit]');   
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
+
+// Iterating the resultList
+var resultxpath = "//*[@id='rso']//h3/span";
+var xpath = await page.$x(resultxpath);
+var resultList = [];
+for(var i= 0;i<xpath.length;i++){
+resultList.push(await page.evaluate(el => el.textContent,xpath[i]));
+}
+console.log(resultList);
+
+//Validating the filetype
+//here i have used string to match the filetype, 
+//it can also be done with gettting the url and checking .pdf in the end with regular expression like in other cases.
+for (var i=0;i<resultList.length;i++){
+  expect(resultList[i]).toMatch("[PDF]");
+}
+
+});
+
+})
+
