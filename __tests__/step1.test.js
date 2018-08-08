c95a2db45511acce85e571bae075f3bfd2f811c5
Piyush Meharban <p.meharban@yahoo.com>
2018-08-08 21:51:29 +0530

    install puppeteer and added test case

--- __tests__/step1.test.js
+++ __tests__/step1.test.js
@@ -10,4 +10,22 @@ beforeEach (async () => {
 afterEach (() => {
     browser.close()
   })
+  
+
+ //Opening the Page and entering the search query 
+test('Title == google', async () => {
+    await page.goto(url);   
+    const title = await page.title();
+    expect(title).toBe("Google");
+    await page.click("input[name=q]");
+    await page.type("input[name=q]", "Botify");
+    await page.evaluate(async() => {
+      await new Promise(function(resolve) { 
+             setTimeout(resolve, 1000)
+      });
+  });
+    await page.click('input[type=submit]');   
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
+
+  });    
 })
