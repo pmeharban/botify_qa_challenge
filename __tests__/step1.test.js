3527eaa4f1bb5ba257cff2d84936a1cb7b00d50b
Piyush Meharban <p.meharban@yahoo.com>
2018-08-08 22:09:09 +0530

    modified test step1

---
+++ __tests__/step1.test.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+jest.setTimeout(30000);
+describe('Open Google', () => {
+  var browser, page;
+  var url = 'https://google.com'
+beforeEach (async () => {
+    browser = await puppeteer.launch({ headless: false });
+    page = await browser.newPage();
+  })
+afterEach (() => {
+    browser.close()
+  })
+  
+
+ //Opening the Page and entering the search query 
+test('Title == google', async () => {
+    await page.goto(url);   
+    const title = await page.title();
+    expect(title).toBe("Google");
+    await page.click("input[name=q]");
+    await page.type("input[name=q]", "Botify");
+    await page.evaluate(async() => {
+      await new Promise(function(resolve) { 
+             setTimeout(resolve, 1000)
+      });
+  });
+    await page.click('input[type=submit]');   
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
+
+  });    
+})
